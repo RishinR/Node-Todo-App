4ee17073aedde858ca420752195124f7f3a9f27f
RishinR <rishinrnew2020@gmail.com>
2023-05-01 21:29:32 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//connect to mongodb
+mongoose.connect('mongodb+srv://Rishin:Rishin%402002@cluster0.cfa3mts.mongodb.net/test', {
+    useNewurlParser: true,
+    useunifiedTopology: true,
+});
+
+//middlewares
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+// routes
+app.use(require('./routes/index'));
+app.use(require('./routes/todo'))
+// listen to server
+app.listen(3000, ()=>{
+    console.log('listening on http://localhost:3000');
+});
