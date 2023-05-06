3fc001d8264dda5be8702de57b0a56318bef5db1
RishinR <rishinrnew2020@gmail.com>
2023-05-06 21:03:09 +0530

    resolved issues with app.js and the csvData conversion function in ./utils/mysheet.js

--- utils/mysheetapi.js
+++ utils/mysheetapi.js
@@ -1,57 +1,52 @@
 const fs = require("fs");
 const { parse } = require("csv-parse");
 const mongoose = require("mongoose");
-const Login = require('../models/Login');
-const bcrypt = require('bcrypt');
-// const MongoClient = require("mongodb").MongoClient;
+const Login = require("../models/Login");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const db = require('./mongo').mongoURI;
-mongoose.connect(db,{
-    useNewurlParser: true,
-    useunifiedTopology: true,
-    family:4
+const db = require("./mongo").mongoURI;
+mongoose.connect(db, {
+  useNewurlParser: true,
+  useunifiedTopology: true,
+  family: 4,
 });
 
-// read value from csv into array
-let l = [];
-fs.createReadStream('../public/sample.csv')
-  .pipe(parse({ delimiter: ",", from_line: 2 }))
-  .on("data", function (row) {
-    // console.log(row);
-    l.push(row);
-  })
-  .on("error", function (error) {
-    console.log(error.message);
-  })
-  .on("end", async function () {
-    // console.log(l)
-    // write to db
-
-    for(i = 0; i < l.length ; ++i){
+async function csvData() {
+  const csvFilePath = __dirname + "/../public/sample.csv"; // use __dirname to get the absolute path
+  let l = [];
+  fs.createReadStream(csvFilePath)
+    .on("error", (err) => {
+      console.error(err);
+    })
+    .pipe(parse({ delimiter: ",", from_line: 2 }))
+    .on("data", function (row) {
+      l.push(row);
+    })
+    .on("error", function (error) {
+      console.log(error.message);
+    })
+    .on("end", async function () {
+      for (i = 0; i < l.length; ++i) {
         const email = l[i][0];
         const pass = l[i][1];
-        console.log(email, pass);
         const password = bcrypt.hashSync(pass, saltRounds);
-        const us = await Login.findOne({ "email": email}, {_id:1});
-        console.log(us);
-        if(us){
+        const us = await Login.findOne({ email: email }, { _id: 1 });
+        if (us) {
           console.log("user already present");
-        }
-        else{
-          const newLogin = new Login({email, password});
-          newLogin.save()
-          .then(() => {
-              console.log('successfully added todo');
-              // res.redirect('/');
-          })
-          .catch((err)=>{
+        } else {
+          const newLogin = new Login({ email, password });
+          newLogin
+            .save()
+            .then(() => {
+              console.log("successfully added todo");
+            })
+            .catch((err) => {
               console.log(err);
-          })
-          console.log(email, password);
+            });
         }
-    
-        // save the todo object
-    }
-    console.log("finished");
-  });
+      }
+      console.log("finished");
+    });
+}
 
+module.exports = { csvData: csvData };
