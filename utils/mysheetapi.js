d377386c04ed06041c3909e1b563f3ee244f2a31
RishinR <rishinrnew2020@gmail.com>
2023-05-06 14:50:23 +0530

    fixed user validation

--- utils/mysheetapi.js
+++ utils/mysheetapi.js
@@ -2,8 +2,9 @@ const fs = require("fs");
 const { parse } = require("csv-parse");
 const mongoose = require("mongoose");
 const Login = require('../models/Login');
+const bcrypt = require('bcrypt');
 // const MongoClient = require("mongodb").MongoClient;
-
+const saltRounds = 10;
 const db = require('./mongo').mongoURI;
 mongoose.connect(db,{
     useNewurlParser: true,
@@ -14,7 +15,7 @@ mongoose.connect(db,{
 // read value from csv into array
 let l = [];
 fs.createReadStream('../public/sample.csv')
-  .pipe(parse({ delimiter: ",", from_line: 1 }))
+  .pipe(parse({ delimiter: ",", from_line: 2 }))
   .on("data", function (row) {
     // console.log(row);
     l.push(row);
@@ -22,26 +23,34 @@ fs.createReadStream('../public/sample.csv')
   .on("error", function (error) {
     console.log(error.message);
   })
-  .on("end", function () {
+  .on("end", async function () {
     // console.log(l)
     // write to db
 
     for(i = 0; i < l.length ; ++i){
         const email = l[i][0];
-        const password = l[i][1];
-        const newLogin = new Login({email, password})
+        const pass = l[i][1];
+        console.log(email, pass);
+        const password = bcrypt.hashSync(pass, saltRounds);
+        const us = await Login.findOne({ "email": email}, {_id:1});
+        console.log(us);
+        if(us){
+          console.log("user already present");
+        }
+        else{
+          const newLogin = new Login({email, password});
+          newLogin.save()
+          .then(() => {
+              console.log('successfully added todo');
+              // res.redirect('/');
+          })
+          .catch((err)=>{
+              console.log(err);
+          })
+          console.log(email, password);
+        }
     
         // save the todo object
-    
-        newLogin.save()
-        .then(() => {
-            console.log('successfully added todo');
-            res.redirect('/');
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-        console.log(email, password);
     }
     console.log("finished");
   });
